fbd7d6972243abbd90cb1d7674ae408745834113
jungyeon <wjddus5533@naver.com>
2024-02-17 18:04:24 +0900

    feat : 프로필 등록 & 기본이미지 등록

---
+++ discord_backend/src/main/java/com/imfreepass/discord/user/api/response/ViewUserImg.java
@@ -0,0 +1,31 @@
+package com.imfreepass.discord.user.api.response;
+
+import java.time.ZonedDateTime;
+
+import com.imfreepass.discord.entity.State;
+import com.imfreepass.discord.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ViewUserImg {
+
+	private Long userImgId; // img PK
+	private Long userId;
+	private String original; // 원본 이름 
+	private String path; // 경로 
+}
